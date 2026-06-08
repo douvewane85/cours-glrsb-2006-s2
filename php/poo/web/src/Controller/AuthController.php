09e77c7a82e131275c2149434cfb1486b604cbb7
seneaurefonte <douvewane85@gmail.com>
2026-06-08 18:09:33 +0000

    Remove unnecessary blank line in AuthController.php

--- php/poo/web/src/Controller/AuthController.php
+++ php/poo/web/src/Controller/AuthController.php
@@ -13,7 +13,6 @@ class AuthController extends Controller{
           return parent::__construct();
         }
    
-  
     public function login():void{
         $errors=[];
         if($_SERVER["REQUEST_METHOD"]=="POST"){
